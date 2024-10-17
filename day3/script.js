6e10370b900205c8188d01fffb4acf3c23ddb4d0
GauravVashist239247 <gvashist1010@gmail.com>
2024-10-17 13:57:14 +0530

    first commit

---
+++ day3/script.js
@@ -0,0 +1,41 @@
+function fun() {
+
+    console.log("hiiiiiiiiiiiiiiiiiii....................");
+
+}
+fun()
+
+function myfun() {
+    alert("i am hiiiiiiiiiiiiiiiiiiiiiiiii ")
+}
+
+
+let hey = 9;
+console.log(typeof (hey));
+
+// const k = 9;
+// k = 30;
+// console.log(k);
+// console.log(typeof (k));
+// for (let i = 0; i < 5; i++) {
+//     console.log("Iteration: " + i);
+// }
+// let i = 0;
+
+
+// while (i < 5) {
+//     console.log("Iteration: " + i);
+//     i++;
+// }
+const arr=[1,2,3];
+console.log(arr);
+console.log(typeof arr);
+let searchValue = 2;
+
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] == searchValue) {
+//         console.log(i);  
+//     }
+// }
+// let i = 0;
